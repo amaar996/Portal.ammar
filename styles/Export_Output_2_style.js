d897ae517a8ed001ddf0805aaaef2e22cc8b7ae5
amaar996 <114784905+amaar996@users.noreply.github.com>
2022-10-01 03:41:11 +0300

    Add files via upload

--- styles/Export_Output_2_style.js
+++ styles/Export_Output_2_style.js
@@ -31,14 +31,14 @@ var style_Export_Output_2 = function(feature, resolution){
         size = 2*(Math.log(size)/ Math.log(2))
     }
     var style = [ new ol.style.Style({
-        image: new ol.style.RegularShape({radius: 3.6666666666666567 + size, points: 5,
-            radius2: 1.8333333333333284, stroke: new ol.style.Stroke({color: 'rgba(184,8,8,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(184,8,8,1.0)'})}),
+        image: new ol.style.RegularShape({radius: 3.66666 + size, points: 5,
+            radius2: 1.83333, stroke: new ol.style.Stroke({color: 'rgba(184,8,8,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(184,8,8,1.0)'})}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     }),new ol.style.Style({
-        image: new ol.style.RegularShape({radius: 3.9999999999999916 + size, points: 5,
-            radius2: 1.9999999999999958, stroke: new ol.style.Stroke({color: 'rgba(255,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(255,0,0,1.0)'})}),
+        image: new ol.style.RegularShape({radius: 4.0 + size, points: 5,
+            radius2: 2.0, stroke: new ol.style.Stroke({color: 'rgba(255,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(255,0,0,1.0)'})}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
